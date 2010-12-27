f326859d81c18767a88176c977138fb0c6795614
Herminio Torres <herminiocesar@gmail.com>
2010-12-27 12:38:18 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bancodehoras::Application.config.secret_token = '635f824c4861e60b7cc00b3aef4493f1054e5b967915e6188554330c7e35584ed0864fe42e66fccce1fa49387b7796e165843ffae395562e5022c4119928e71c'
